c1be8801bf284094a9a2ca2bd709761e192e9d4a
Malted <59726149+ma1ted@users.noreply.github.com>
2021-11-22 08:59:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function falsy(obj, options) {
   options = options || { ws: false };
   
   if(!!options.ws && !!obj && typeof obj === 'string') {
-    return !!!obj.trim();
+    return !obj.trim();
   }
 
   return !!!obj;
